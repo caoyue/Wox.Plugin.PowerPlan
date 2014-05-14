762633e2027e21968ee8c16764a5f6e56b4d10c7
caoyue <i@caoyue.me>
2014-05-15 00:44:00 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Wox.Plugin.PowerPlan
 ====================
-
 quickly switch between power plans
+
+![example](http://ww2.sinaimg.cn/large/3e69b0ccgw1ege9rb5wsjj20nv0fladk.jpg)
+
---
+++ Wox.Plugin.PowerPlan/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wox.Plugin.PowerPlan
+{
+    public class Main : IPlugin
+    {
+        private static string currentPath;
+        public void Init(PluginInitContext context)
+        {
+            currentPath = context.CurrentPluginMetadata.PluginDirecotry;
+        }
+
+        public List<Result> Query(Query query)
+        {
+            var results = new List<Result>();
+            var powerList = PowerManager.GetAll();
+            results = powerList.Select(p => new Result() {
+                Title = p.Name,
+                SubTitle = p.Name,
+                IcoPath = System.IO.Path.Combine(currentPath, p.IsActive ? @"ico\Battery-2.png" : @"ico\Battery-1.png"),
+                Action = (a) => {
+                    PowerManager.Active(p.Id);
+                    return true;
+                }
+            }).ToList();
+            if (query.ActionParameters.Any()) {
+                var keyword = query.ActionParameters[0].ToLower();
+                results = results.Where(r => r.Title.ToLower().Contains(keyword)).ToList();
+            }
+            return results;
+        }
+    }
+}
